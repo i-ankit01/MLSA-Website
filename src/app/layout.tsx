878a6549847feb16d83bd0b172c7bdafcd9588ad
i-ankit01 <ankit.common.acc@gmail.com>
2025-08-09 23:48:20 +0530

    add change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased p-4 md:px-12`}
       >
         {children}
       </body>
